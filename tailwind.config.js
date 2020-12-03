320768b2ac683b13f128fab2fb2250d82a14b86c
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2020-12-03 15:59:05 -0800

    Fix overflows, add rep/fishy gradient bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,10 @@ module.exports = {
           900: '#161d28',
           800: '#5a6171',
         }
+      },
+      boxShadow: {
+        DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.5), 0 1px 2px 0 rgba(0, 0, 0, 0.5)',
+        md: '0 4px 6px -1px rgba(0, 0, 0, 0.5), 0 2px 4px -1px rgba(0, 0, 0, 0.6)',
       }
     },
     fontFamily: {
