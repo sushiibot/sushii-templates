41428dc409d2cb6f9efcbb217abb5d3cc7bd736c
drklee3 <derrick@dlee.dev>
2020-12-02 21:11:04 -0800

    Add prod/purge configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: [],
+  purge: [
+    "./src/**/*.hbs"
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
